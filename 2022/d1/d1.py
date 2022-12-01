39497d6046d79cc6e3457d5dff141b168b199220
bytesontherocks <bytes@dimoni>
2022-12-01 08:26:13 +0000

    day1 ex1 and 2

--- 2022/d1/d1.py
+++ 2022/d1/d1.py
@@ -1,16 +1,30 @@
 
-        
-def calculate_ex1(input_file_name):
+def getElfCalListSorted(input_file_name):
     f = open(input_file_name, "r")
-    return f.readlines()
-    
+    lines = f.readlines()
+    num_elfs = 0
+    cal_elfs= [0]
+    num_cal = 0
+    for line in lines[0:len(lines)-1]:
+        if line != "\n":
+            num_cal += int(line)
+        else:            
+            num_elfs += 1
+            cal_elfs.append(num_cal)
+            num_cal = 0
+    cal_elfs.sort()
+    return cal_elfs
 
+def calculate_ex1(input_file_name):
+    cal_elfs = getElfCalListSorted(input_file_name)
+    return cal_elfs[len(cal_elfs)-1]
+        
+    
+def calculate_ex2(input_file_name):
+    cal_elfs = getElfCalListSorted(input_file_name)
+    return cal_elfs[len(cal_elfs)-1] +  cal_elfs[len(cal_elfs)-2] +  cal_elfs[len(cal_elfs)-3]
 
 if __name__ == "__main__":
-    lines = calculate_ex1("input.txt")
-    count = 0
-    # Strips the newline character
-    #for line in lines:
-    for line in lines[0:len(lines)-1]:
-        count += 1
-        print("Line{}: {}".format(count, line.strip()))
+    res1 = calculate_ex1("input.txt")
+    res2 = calculate_ex2("input.txt")
+    print(f"ex1: {res1}, ex2: {res2}")
