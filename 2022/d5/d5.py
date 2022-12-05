53be775f5dded7f1d90f761a7cec621cc75dc23e
bytesontherocks <bytesontherocks@gmail.com>
2022-12-05 21:02:34 +0000

    parsing taking too long
    I don't find the right tools

--- 2022/d5/d5.py
+++ 2022/d5/d5.py
@@ -3,8 +3,32 @@ import queue
 def calculateScoreEx1(input_file_name):
     f = open(input_file_name, "r")
     lines = f.readlines()
+    ln_stack =[]
+    l_ix = 0
+    for line in lines:
+        # parse stacks       
+        if line == "\n":
+            break
+        l_ix += 1
+        ln_stack.append(line)
 
-    q = queue.LifoQueue()
+    num_qs= int(ln_stack[-1][-3])#last element
+    max_stack = len(ln_stack) - 1
+    #print(num_qs)
+    #print(max_stack)
+    # print(f"s1: {ln_stack[-2][1]} s2: {ln_stack[-2][5]}, s3: {ln_stack[-2][9]}")
+    for q in range(0, num_qs):
+        for el_ix in range(0, max_stack):
+            el = ln_stack[-el_ix-2][1 + (q * 4)]
+            if el != "":
+                print(el)
+
+    # print(ln_stack)
+
+
+    # parse moves
+    
+    #q = queue.LifoQueue()
 
     score = 0
     
@@ -22,6 +46,6 @@ def calculateScoreEx2(input_file_name):
 
         
 if __name__ == "__main__":
-    res1 = calculateScoreEx1("input.txt")
+    res1 = calculateScoreEx1("input_sample.txt")
     res2 = calculateScoreEx2("input.txt")
     print(f"ex1: {res1}, ex2: {res2}")
