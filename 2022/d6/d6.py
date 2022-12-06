6eed18419c5dbf1d3bb4e02f0e217d0aad88fc39
bytesontherocks <bytesontherocks@gmail.com>
2022-12-06 09:43:41 +0000

    tidy up

--- 2022/d6/d6.py
+++ 2022/d6/d6.py
@@ -1,37 +1,26 @@
+def getMarkerPosition(input, marker_size):
+    num_it = marker_size
+    for ch in range(0,len(input)-marker_size):
+        marker = ""
+        for x in range(0,marker_size):
+            marker += input[ch + x]
 
+        if len(set(marker)) == marker_size:
+            print(marker)
+            break
+        num_it += 1
+
+    return num_it
 
 def calculateScoreEx1(input_file_name):    
     f = open(input_file_name, "r")
     lines = f.readlines()
-    input = lines[0]
-
-    num_it = 4
-    for ch in range(0,len(input)-4):
-        marker = input[ch] + input[ch+1] +input[ch+2] + input[ch+3]
-        if len(set(marker)) == 4:
-            print(marker)
-            break
-        num_it += 1
-        
-    return num_it
+    return getMarkerPosition(lines[0], 4)
 
 def calculateScoreEx2(input_file_name):    
     f = open(input_file_name, "r")
-    lines = f.readlines()
-    input = lines[0]
-
-    num_it = 14
-    for ch in range(0,len(input)-14):
-        marker = ""
-        for x in range(0,14):
-            marker += input[ch + x]
-
-        if len(set(marker)) == 14:
-            print(marker)
-            break
-        num_it += 1
-        
-    return num_it
+    lines = f.readlines()  
+    return getMarkerPosition(lines[0], 14)
 
         
 if __name__ == "__main__":
