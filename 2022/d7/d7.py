a4134534a9c355dd983b1e88cbd4b5016609cb30
bytesontherocks <bytesontherocks@gmail.com>
2022-12-08 23:43:23 +0000

    playing

--- 2022/d7/d7.py
+++ 2022/d7/d7.py
@@ -1,61 +1,78 @@
 def getNextLine(lines, ix):
     return lines[ix].split(), ix + 1
-    return 0
 
 def getDirectorySize(tree, dir_id):
     dir=tree[dir_id]
-    dir_sz=0
-    for d in dir:
-        if d.isalpha():
-            dir_sz += getDirectorySize(tree, d)
-        else:
-            dir_sz += d
+    dir_sz=dir[-1]
+    for d in range(0,len(dir)-1):  
+        dir_sz += getDirectorySize(tree, dir[d])
     
     return dir_sz
-    
-def getDirectoriesSize(tree):
-    dir = tree['/']
-    list_d_sz=[]
-    dir_sz=0
-    for d in dir:
-        if d.isalpha():
-            dir_sz += getDirectorySize(tree, d)
-        else:
-            dir_sz += d
-        list_d_sz.append(dir_sz)
-        
-    return list_d_sz
+
+def updateDirPath(ln, dir):
+    if ln[1] == "cd" and ln[2].isalpha():
+        dir.append(ln[2])
+    elif  ln[1] == "cd" and ln[2]=="..":
+        dir.pop() # cd ..
+    print(dir)
+    return dir
+
+    root_dir = dir['/']
+    root_dir = [{'a' : [{'e': [584]}, f, g, h]}, b, c, {'d': [j,dl,de, k]}]
+
+    # dir a -> C
+    # 14848514 b.txt
+    # 8504156 c.dat
 
 
 def calculateScoreEx1(input_file_name):
     with open(input_file_name, "r") as f:
         lines = f.read().splitlines()
-        tree = {} # contains all the directories
         ln_ix=0 
         while ln_ix < len(lines):     
             ln, ln_ix = getNextLine(lines, ln_ix)
-            if ln[1] == "ls":
-                dir_name = lines[ln_ix - 2].split()[2]
-                ln, ln_ix = getNextLine(lines, ln_ix)
+            root = []      
+
+            last_line = False
+
+            while ln[1] != "cd" or ln[2]!= "..":
+                entry = {}
                 dir = []
-                acc_size = 0
-                while ln[0] != "$" and ln_ix < len(lines):
-                    if ln[0] == "dir":
-                        dir.append(ln[1])
-                    elif ln[0].isdigit():
-                        acc_size += int(ln[0])
+                cwd = ""
+                if ln[1] == "cd":    
+                    cwd = ln[2]          
+                    ln, ln_ix = getNextLine(lines, ln_ix)   
+                    ln, ln_ix = getNextLine(lines, ln_ix)                
+                    last_line = False
+                    acc_size = 0
+                    while ln[0] != "$" and last_line == False:                    
+                        if ln[0] == "dir":
+                            nested_dir = {}
+                            nested_dir[ln[1]] = []
+                            dir.append(nested_dir)
+                        elif ln[0].isdigit():              
+                            acc_size += int(ln[0])
 
+                        if ln_ix < len(lines):
+                            ln, ln_ix = getNextLine(lines, ln_ix)
+                        else:
+                            last_line = True
+
+                    dir.append(acc_size)
+
+                if ln_ix < len(lines):
                     ln, ln_ix = getNextLine(lines, ln_ix)
+                else:
+                    last_line = True
 
-                dir.append(acc_size)
-                tree[dir_name]= dir
+                entry[cwd] = dir
+                print(f"entry: {entry}")
 
-        print(f"tree: {tree}")
+            #root.append(entry)
 
-        sz = getDirectoriesSize(tree)
-        # dir_size=[]
-        # for dir in tree:
-        #     dir_size.append(getDirectorySize(tree, "/"))
+
+            #print(f"dir: {dir}")
+   
 
     return 0
 
