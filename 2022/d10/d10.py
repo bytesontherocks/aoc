f99aa930734ca2622d7ad284fc5de36fb5c421cd
bytesontherocks <bytesontherocks@gmail.com>
2022-12-10 14:16:19 +0000

    ex2 completed

--- 2022/d10/d10.py
+++ 2022/d10/d10.py
@@ -1,33 +1,31 @@
 
 
 
-def getX(cycle):
-    return 0
-
-def calculateScoreEx1(input_file_name):    
+def getList(lines):
     reg = []
     x=1
-    cycle=0
     reg.append(x)
+    for ln in lines:
+        ln = ln.split()
+
+        if ln[0] == "addx":
+            reg.append(x)
+            reg.append(x)
+            x += int(ln[1])
+        else:
+            reg.append(x)
+    return reg
+
+def calculateScoreEx1(input_file_name):    
+
     with open(input_file_name, "r") as f:
         lines = f.read().splitlines()
-        for ln in lines:
-            ln = ln.split()
-            cycle+=1
+        reg = getList(lines)
 
-            if ln[0] == "addx":
-                reg.append(x)
-                cycle+=1
-                reg.append(x)
-                x += int(ln[1])
-            else:
-                reg.append(x)
-                
     th=[20,60,100,140,180,220]
     sum=0
     for val in th:
         sum+=reg[val]*val
-        print(f"reg: {reg[val]} val: {val}")
 
     print(sum)
         
@@ -41,8 +39,28 @@ def calculateScoreEx1(input_file_name):
 
 
 def calculateScoreEx2(input_file_name):    
-    f = open(input_file_name, "r")
-    lines = f.readlines()  
+    with open(input_file_name, "r") as f:
+        lines = f.read().splitlines()
+        reg = getList(lines)
+        print(reg)
+        print(f"len_reg{len(reg)}")
+        crt = []
+
+        for ix in range(0,6):
+            crt_ln=''        
+            for crt_pos in range(0, 40):
+                cycle = 1+crt_pos + ix*40
+                rex_x = reg[cycle] - 1
+                if crt_pos < rex_x+3 and crt_pos >=rex_x:
+                    crt_ln+='#'
+                else:
+                    crt_ln+='.'
+
+            crt.append(crt_ln)
+
+        for c in crt:
+            print(c)
+
     return 0
 
 if __name__ == "__main__":
