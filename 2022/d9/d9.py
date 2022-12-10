66efe63f1e4575c723e7e0967849b6a8cb133353
bytesontherocks <bytesontherocks@gmail.com>
2022-12-10 22:09:59 +0000

    d9 ex2 solved

--- 2022/d9/d9.py
+++ 2022/d9/d9.py
@@ -1,4 +1,10 @@
-def updateTailPos(xH,yH,xT,yT, pairsT) -> tuple:
+def updateTailPos(tH,tT, pairsT) -> tuple:
+
+    xH=tH[0]
+    yH=tH[1]
+    xT=tT[0]
+    yT=tT[1]
+
     if xH == xT and yH == yT:
         return xT,yT
 
@@ -45,38 +51,91 @@ def calculateScoreEx1(input_file_name):
         pairs.append((x, y))
         pairsT.append((xT, yT))
         for ln in lines:
-            ln = ln.split()
-                
+            ln = ln.split()             
            
             if ln[0] == 'R':
                 for p in range(0,int(ln[1])):
                     x += 1
                     pairs.append((x, y))
-                    xT,yT= updateTailPos(x,y,xT,yT, pairsT)
+                    xT,yT= updateTailPos((x, y),(xT,yT), pairsT)
                 
             elif ln[0] == 'L':
                 for p in range(0,int(ln[1])):
                     x -= 1
                     pairs.append((x, y))
-                    xT,yT= updateTailPos(x,y,xT,yT, pairsT)
+                    xT,yT= updateTailPos((x, y),(xT,yT), pairsT)
                
             elif ln[0] == 'U':
                 for p in range(0,int(ln[1])):
                     y += 1
                     pairs.append((x, y))
-                    xT,yT= updateTailPos(x,y,xT,yT, pairsT)
+                    xT,yT= updateTailPos((x, y),(xT,yT), pairsT)
                
             elif ln[0] == 'D':
                 for p in range(0,int(ln[1])):
                     y -= 1
                     pairs.append((x, y))
-                    xT,yT= updateTailPos(x,y,xT,yT, pairsT)
+                    xT,yT= updateTailPos((x, y),(xT,yT), pairsT)
                         
     return len(set(pairsT))
 
 
 def calculateScoreEx2(input_file_name):
-    return 0
+    with open(input_file_name, "r") as f:
+        lines = f.read().splitlines()
+        x = 0
+        y = 0
+
+        knots_points=[]#list of visited coordinates
+        knots_xy = []#current coordinates
+        num_knots=10
+       
+        for k in range(0,num_knots):
+            alist=[]
+            knots_points.append(alist)
+            knots_xy.append((0, 0))
+       
+
+        for ln in lines:
+            ln = ln.split()                
+           
+            if ln[0] == 'R':
+                for p in range(0,int(ln[1])):
+                    x=knots_xy[0][0]#move the head
+                    x+=1
+                    knots_xy[0] = (x,knots_xy[0][1])
+                    for k in range(0,num_knots-1):
+                        knots_points[k].append(knots_xy[k])
+                        knots_xy[k+1]= updateTailPos(knots_xy[k], knots_xy[k+1], knots_points[k+1])
+                
+            elif ln[0] == 'L':
+                for p in range(0,int(ln[1])):
+                    x=knots_xy[0][0]#move the head
+                    x-=1
+                    knots_xy[0] = (x,knots_xy[0][1])
+                    for k in range(0,num_knots-1):
+                        knots_points[k].append(knots_xy[k])
+                        knots_xy[k+1]= updateTailPos(knots_xy[k], knots_xy[k+1], knots_points[k+1])
+               
+            elif ln[0] == 'U':
+                for p in range(0,int(ln[1])):
+                    y=knots_xy[0][1]#move the head
+                    y+=1
+                    knots_xy[0] = (knots_xy[0][0],y)
+                    for k in range(0,num_knots-1):
+                        knots_points[k].append(knots_xy[k])
+                        knots_xy[k+1]= updateTailPos(knots_xy[k], knots_xy[k+1], knots_points[k+1])
+               
+            elif ln[0] == 'D':
+                for p in range(0,int(ln[1])):
+                    y=knots_xy[0][1]#move the head
+                    y-=1
+                    knots_xy[0] = (knots_xy[0][0],y)
+                    for k in range(0,num_knots-1):
+                        knots_points[k].append(knots_xy[k])
+                        knots_xy[k+1]= updateTailPos(knots_xy[k], knots_xy[k+1], knots_points[k+1])
+                        
+    return len(set(knots_points[-1]))
 
 if __name__ == "__main__":
     res1 = calculateScoreEx1("input.txt")
