887c8c1ea14c21440383a2e01d77eaa7a12a5dac
bytesontherocks <bytesontherocks@gmail.com>
2022-12-10 21:17:42 +0000

    ex 1 tidy up

--- 2022/d9/d9.py
+++ 2022/d9/d9.py
@@ -5,12 +5,12 @@ def updateTailPos(xH,yH,xT,yT, pairsT) -> tuple:
     if yH == yT:
         if xH>xT and (xH-xT)>1:
             xT+=1
-        elif xT>xT and (xT-xH)>1:
+        elif xT>xH and (xT-xH)>1:
             xT-=1
     elif xH == xT:
         if yH>yT and (yH-yT)>1:
             yT+=1
-        elif yT>yT and (yT-yH)>1:
+        elif yT>yH and (yT-yH)>1:
             yT-=1
     elif abs(xH-xT)>1 or abs(yH-yT)>1:
         if xH>xT and yH>yT:
@@ -26,26 +26,12 @@ def updateTailPos(xH,yH,xT,yT, pairsT) -> tuple:
             xT-=1
             yT+=1
 
-        #print("diagonal")
-    
     pairsT.append((xT,yT))
-    print(f"Head: {(xH,yH)}, Tail{(xT,yT)}")
 
     return xT,yT
 
 
 def calculateScoreEx1(input_file_name):    
-
-    # ln = ['']
-    # for x in range(0,15):    
-    #     line=''            
-    #     for y in range(0,15):
-    #         line += '.'
-
-    #     ln.append(line)
-
-    # for l in ln:
-    #     print(l)     
 
     with open(input_file_name, "r") as f:
         lines = f.read().splitlines()
@@ -85,16 +71,7 @@ def calculateScoreEx1(input_file_name):
                     y -= 1
                     pairs.append((x, y))
                     xT,yT= updateTailPos(x,y,xT,yT, pairsT)
-                
-        
-        # print("Head: ")
-        # for p in pairs:
-        #     print(p)
-        
-        # print("Tail: ")
-        for p in set(pairsT):
-             print(p)
-        
+                        
     return len(set(pairsT))
 
 
@@ -102,6 +79,6 @@ def calculateScoreEx2(input_file_name):
     return 0
 
 if __name__ == "__main__":
-    res1 = calculateScoreEx1("input_sample.txt")
+    res1 = calculateScoreEx1("input.txt")
     res2 = calculateScoreEx2("input.txt")
     print(f"ex1: {res1}, ex2: {res2}")
