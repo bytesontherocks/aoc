801171eb5012cab8d7aea248ab8b8d729541c51b
bytesontherocks <bytesontherocks@gmail.com>
2022-12-11 22:07:39 +0000

    Update d11.py

--- 2022/d11/d11.py
+++ 2022/d11/d11.py
@@ -3,7 +3,8 @@ import re
 
 class Monkey:
     worry_items=[]
-    worry_new=[]
+    worry_new_operand=0
+    worry_new_operation=0
     test=1
     test_true=1
     test_false=1
@@ -11,23 +12,30 @@ class Monkey:
 
     def __init__(self,worry_items, worry_new, test, t_true,t_false):
         self.worry_items=worry_items
-        self.worry_new=worry_new
         self.test=test
         self.test_true=t_true
-        self.test_false=t_false
+        self.test_false=t_false        
+        if worry_new[0].isdigit():
+            self.worry_new_operand = int(worry_new[0])
+            if worry_new[1] == '+':
+                self.worry_new_operation=1
+            else:
+                self.worry_new_operation=2
+        
+
     
     def printList(self):
         for it in self.worry_items:
             print(it)
     
     def calculateNew(self, old) -> int:
-        if self.worry_new[0].isalpha():
-            return int(old * old)
-        if self.worry_new[1] == '+':
-            return int(self.worry_new[0]) + old
-        if self.worry_new[1] == '*':
-            return int(self.worry_new[0]) * old
-    
+        if self.worry_new_operation==0:
+            return old * old
+        elif self.worry_new_operation == 1:
+            return self.worry_new_operand + old
+        else:
+            return self.worry_new_operand * old
+
     def addWorryItem(self, item):
         self.worry_items.append(item)
     
@@ -43,13 +51,23 @@ class Monkey:
             self.inspection+=1
 
         self.worry_items.clear()#items processed
+    
+    def inspect_ex2(self, monkeys):
+        m1=monkeys[self.test_true]
+        m2=monkeys[self.test_false]
+        for it in self.worry_items:
+            new=self.calculateNew(it)
+            if new%self.test==0:
+                m1.addWorryItem(new)
+            else:
+                m2.addWorryItem(new)
+
+        
+        self.inspection+=len(self.worry_items)
+        self.worry_items.clear()#items processed
 
     def getInspectedItems(self) -> int:
         return self.inspection
-
-
-
-
 
 
 def calculateScoreEx1(input_file_name):    
@@ -63,13 +81,10 @@ def calculateScoreEx1(input_file_name):
         while ln_ix<len(lines):
             ln=lines[ln_ix]
            
-            if "Monkey" in ln:             
-                mIx=ln[-2]
+            if "Monkey" in ln:   
                 ln=lines[ln_ix+1]
                 alist=[]
                 alist = re.findall(r'\d+',ln)
-                # for it in alist:
-                #     print(it)
                 monkeys_it.append(alist)
                 
                 ln=lines[ln_ix+2].split()
@@ -80,7 +95,6 @@ def calculateScoreEx1(input_file_name):
                 test=re.findall(r'\d+',lines[ln_ix+3])                
                 test_t=re.findall(r'\d+',lines[ln_ix+4])
                 test_f=re.findall(r'\d+',lines[ln_ix+5])
-                #print(f"test:{test[0]}, ttrue{test_t}, tfalse{test_f}")
                 
                 m=Monkey(alist,op,int(test[0]),int(test_t[0]),int(test_f[0]))  
                 monkeys.append(m)
@@ -88,7 +102,7 @@ def calculateScoreEx1(input_file_name):
             ln_ix+=1
         
         num_rounds=20
-        for rounds in range(0,20):
+        for rounds in range(0,num_rounds):
             for m in monkeys:
                 m.inspect(monkeys)
             rounds+=1
@@ -99,20 +113,63 @@ def calculateScoreEx1(input_file_name):
             print(f"monkey {m} : {monkeys[m].getInspectedItems()}")
             inspected.append(monkeys[m].getInspectedItems())
         inspected.sort()
-                
-            
-        
+        print(inspected)
+                      
     return inspected[-1]*inspected[-2]
 
 
 def calculateScoreEx2(input_file_name):    
     with open(input_file_name, "r") as f:
         lines = f.read().splitlines()
-       
+        monkeys_it = []
+        monkeys=[]
 
-    return 0
+        ln_ix=0
+        while ln_ix<len(lines):
+            ln=lines[ln_ix]
+           
+            if "Monkey" in ln:             
+                mIx=ln[-2]
+                ln=lines[ln_ix+1]
+                alist=[]
+                alist2 = re.findall(r'\d+',ln)
+                for it in alist2:
+                    alist.append(int(it))
+                    
+                monkeys_it.append(list(map(int, alist)))
+                
+                ln=lines[ln_ix+2].split()
+                op=[]
+                op.append(ln[-1])
+                op.append(ln[-2])
+                
+                test=re.findall(r'\d+',lines[ln_ix+3])                
+                test_t=re.findall(r'\d+',lines[ln_ix+4])
+                test_f=re.findall(r'\d+',lines[ln_ix+5])
+                
+                m=Monkey(alist,op,int(test[0]),int(test_t[0]),int(test_f[0]))  
+                monkeys.append(m)
+            
+            ln_ix+=1
+        
+        num_rounds=1000
+        for rounds in range(0,num_rounds):
+            for m in monkeys:
+                m.inspect_ex2(monkeys)
+            rounds+=1
+        
+        m=0
+        inspected=[]
+        print("---------------")
+        print("Ex2:")
+        for m in range(0,len(monkeys)):
+            print(f"monkey {m} : {monkeys[m].getInspectedItems()}")
+            inspected.append(monkeys[m].getInspectedItems())
+        inspected.sort()
+                      
+    return inspected[-1]*inspected[-2]
 
 if __name__ == "__main__":
-    res1 = calculateScoreEx1("input.txt")
-    res2 = calculateScoreEx2("input.txt")
+    res1 = calculateScoreEx1("input_sample.txt")
+    res2 = calculateScoreEx2("input_sample.txt")
     print(f"ex1: {res1}, ex2: {res2}")
