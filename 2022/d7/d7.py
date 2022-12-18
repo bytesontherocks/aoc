08945241caea01c53b6237e36c8a76beca97cb3e
bytesontherocks <bytes@dimoni>
2022-12-18 15:57:50 +0000

    d7 ex2 solved

--- 2022/d7/d7.py
+++ 2022/d7/d7.py
@@ -31,16 +31,16 @@ def calculateScoreEx1(input_file_name):
         dirs_ix = 0
         current_dir = dirs_ix
         parent=dirs_ix
-        root = parent
+        total_sz_todo = 0
         while ln_ix < len(lines):
             ln = lines[ln_ix].split()      
 
             if ln[1] == "cd":     
                 if ln[2].isalpha() and ln[2]!='/':
-                    dirs.append(Dir(ln[2],current_dir))                     
+                    dirs.append(Dir(ln[2],parent))      
+                    parent=current_dir               
                     dirs_ix+=1
-                    dirs[parent].addDir(dirs_ix)                    
-                    parent=dirs_ix                
+                    dirs[parent].addDir(dirs_ix)                  
                     current_dir = dirs_ix
                 if ln[2] == "..":
                     current_dir=dirs[current_dir].getParent()
@@ -48,9 +48,16 @@ def calculateScoreEx1(input_file_name):
 
             if ln[0].isdigit():
                 dirs[current_dir].accSize(int(ln[0]))
+                total_sz_todo+=int(ln[0])
+
             ln_ix+=1 
             if ln_ix >= len(lines):
                 break
+
+        unused_space=70000000-dirs[0].getSize(dirs)
+        del_dir_sz=30000000-unused_space
+        print(f"unused_space {unused_space}, necessary del space {del_dir_sz}")
+        print(f"dir0 {dirs[0].getSize(dirs)}, calculated total size {total_sz_todo}")   
 
         res_ex1=0
         for d in dirs: 
@@ -69,15 +76,16 @@ def calculateScoreEx2(input_file_name):
         dirs_ix = 0
         current_dir = dirs_ix
         parent=dirs_ix
+        total_sz_todo=0
         while ln_ix < len(lines):
             ln = lines[ln_ix].split()      
 
             if ln[1] == "cd":     
                 if ln[2].isalpha() and ln[2]!='/':
-                    dirs.append(Dir(ln[2],current_dir))                     
+                    dirs.append(Dir(ln[2],current_dir))  
+                    parent=current_dir                   
                     dirs_ix+=1
-                    dirs[parent].addDir(dirs_ix)                    
-                    parent=dirs_ix                
+                    dirs[parent].addDir(dirs_ix)                     
                     current_dir = dirs_ix
                 if ln[2] == "..":
                     current_dir=dirs[current_dir].getParent()
@@ -85,6 +93,8 @@ def calculateScoreEx2(input_file_name):
 
             if ln[0].isdigit():
                 dirs[current_dir].accSize(int(ln[0]))
+                total_sz_todo+=int(ln[0])
+
             ln_ix+=1 
             if ln_ix >= len(lines):
                 break
@@ -92,8 +102,8 @@ def calculateScoreEx2(input_file_name):
         unused_space=70000000-dirs[0].getSize(dirs)
         del_dir_sz=30000000-unused_space
         print(f"unused_space {unused_space}, necessary del space {del_dir_sz}, dir0 {dirs[0].getSize(dirs)}")
-        
-        res_ex2=0
+        print(f"dir0 {dirs[0].getSize(dirs)}, calculated total size {total_sz_todo}")
+
         dirs_sz =[]
         sec_try=[]
         for d in dirs: 
@@ -105,6 +115,8 @@ def calculateScoreEx2(input_file_name):
         dirs_sz.sort()
 
         print(dirs_sz)
+        
+        res_ex2=0
         for d_sz in dirs_sz:
             if d_sz>=del_dir_sz:
                 res_ex2=d_sz
@@ -115,6 +127,6 @@ def calculateScoreEx2(input_file_name):
     return res_ex2
 
 if __name__ == "__main__":
-    res1 = calculateScoreEx1("input_sample.txt")
-    res2 = calculateScoreEx2("input_sample.txt")
+    res1 = calculateScoreEx1("input.txt")
+    res2 = calculateScoreEx2("input.txt")
     print(f"ex1: {res1}, ex2: {res2}")
