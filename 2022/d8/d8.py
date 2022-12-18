c31ca2c45f6479b32d114761922490e69f83624a
bytesontherocks <bytes@dimoni>
2022-12-18 18:33:39 +0000

    d8 ex2 resolved

--- 2022/d8/d8.py
+++ 2022/d8/d8.py
@@ -8,9 +8,7 @@ def calculateScoreEx1(input_file_name):
         # calculate edges
         num_of_ch_in_ln=len(lines[0])
         num_of_lines=len(lines)
-        print(f"chars in ln {num_of_ch_in_ln}, lines {num_of_lines}")
         num_trees_visible=num_of_ch_in_ln*2+num_of_lines*2-4#4 trees are counted twice
-        print(f"trees at the edge {num_trees_visible}")
 
         # create columns
         columns=[]
@@ -20,29 +18,22 @@ def calculateScoreEx1(input_file_name):
             for ln in range(0,len(lines)):
                 columns[ch_ix].append(lines[ln][ch_ix])
         
-        print("columns")
-        print(columns)
-       
         # work out visibility
         for ln in range(1,len(lines)-1):
             for ch_ix in range(1,len(lines[0])-1):
                 el=lines[ln][ch_ix]
-                #print(lines[ln][ch_ix])
                 left=lines[ln][:ch_ix]
                 right=lines[ln][ch_ix+1:]
                 up=columns[ch_ix][:ln]
                 down=columns[ch_ix][ln+1:]
-                #print(f"left {left}, right {right}")
-                #print(f"up {up}, down {down}")
                 vis=False
-                print(max(list(left)))
-                if max(list(left))>el:
+                if max(list(left))<el:
                     vis=True
-                elif max(list(right))>el:
+                elif max(list(right))<el:
                     vis=True
-                elif max(list(up))>el:
+                elif max(list(up))<el:
                     vis=True
-                elif max(list(down))>el:
+                elif max(list(down))<el:
                     vis=True
 
                 if vis==True:
@@ -51,10 +42,55 @@ def calculateScoreEx1(input_file_name):
     return num_trees_visible
 
 def calculateScoreEx2(input_file_name):    
-  
-    return 0
+    with open(input_file_name, "r") as f:
+        lines = f.read().splitlines()       
+
+        # create columns
+        columns=[]
+       
+        for ch_ix in range(0,len(lines[0])):
+            columns.append([])
+            for ln in range(0,len(lines)):
+                columns[ch_ix].append(lines[ln][ch_ix])
+        
+        # work out visibility
+        best_tree=[]
+        for ln in range(1,len(lines)-1):
+            for ch_ix in range(1,len(lines[0])-1):
+                el=lines[ln][ch_ix]
+                left=lines[ln][:ch_ix]
+                right=lines[ln][ch_ix+1:]
+                up=columns[ch_ix][:ln]
+                down=columns[ch_ix][ln+1:]
+                
+                ql=0
+                for l in reversed(list(left)):
+                    ql+=1
+                    if l>=el:
+                        break
+                qr=0
+                for r in list(right):
+                    qr+=1
+                    if r>=el:
+                        break
+                qu=0
+                for u in reversed(list(up)):
+                    qu+=1
+                    if u>=el:
+                        break
+                qd=0
+                for d in list(down):
+                    qd+=1
+                    if d>=el:
+                        break
+                tree_q=ql*qr*qu*qr  
+                best_tree.append(tree_q) 
+
+        best_tree.sort()
+
+    return best_tree[-1]
 
 if __name__ == "__main__":
-    res1 = calculateScoreEx1("input_sample.txt")
-    res2 = calculateScoreEx2("input.txt")
+    res1 = calculateScoreEx1("input.txt")
+    res2 = calculateScoreEx2("input_sample.txt")
     print(f"ex1: {res1}, ex2: {res2}")
