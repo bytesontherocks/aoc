f84549cd4819dabc947df7bfe7bdb4701b203e67
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-12-04 22:17:35 +0000

    ex2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -11,33 +11,34 @@ def calculate_ex(input_file_name):
         print("////")
         n = l.split()
         n_l = [int(i) for i in n]
-        min_nl = min(n_l) - 1
-        max_nl = max(n_l) + 1
+        
         min_s = True
         max_s = True
         dampener_applied_min = False
         dampener_applied_max = False   
-    
-        for i in n_l:
-            d = abs(min_nl - i)
-            if i > min_nl and d <= 3:
-                min_nl = i
+
+        inc_nl = n_l[0]
+        dec_nl = inc_nl
+        for i in n_l[1:]:
+            d = abs(inc_nl - i)
+            if i > inc_nl and d <= 3:
+                inc_nl = i
             else:                
                 if  d <= 1  and dampener_applied_min == False:
-                    min_nl = i
-                    dampener_applied_min = True
-                    print("damped min")
+                    print(f"damped min i {i}, inc_nl {inc_nl}")
+                    inc_nl = i
+                    dampener_applied_min = True                   
                 else:
                     min_s = False
 
-            d = abs(i - max_nl)
-            if i < max_nl and d <= 3:
-                max_nl = i
+            d = abs(i - dec_nl)
+            if i < dec_nl and d <= 3:
+                dec_nl = i
             else:                
                 if  d <= 1  and dampener_applied_max == False:
-                    max_nl = i
+                    print(f"damped max i {i}, dec_nl {dec_nl}")
+                    dec_nl = i
                     dampener_applied_max = True
-                    print("damped max")
                 else:
                     max_s = False
 
@@ -45,10 +46,7 @@ def calculate_ex(input_file_name):
         if min_s or max_s:
             safe = True
             e1 +=1
-            print(n_l)
-            print("////")
-
-        
+        print(n_l)        
         print(safe)
 
     e2 = 0
