4417e6c6e9da2f94325396f9e63d40d7389e51ea
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-12-05 00:07:38 +0000

    another approach for ex2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -23,13 +23,49 @@ def calculate_ex(input_file_name):
         d = []
         for i in n_l[1:]:
             delta = abs(i  - last)
-            d.append(delta <= 3 and delta > 0)
+            d.append(delta in range(1, 4))
             last = i
         if all(d):
             e1 += 1
  
 
-    e2 = 0
+    e2 = e1
+
+    for l in lines:
+    
+        n = l.split()
+        n_l = [int(i) for i in n]
+
+        d=[]
+        last = n_l[0]
+        for i in n_l[1:]:
+            d.append(i - last)
+            last = i
+        # if all(num in range (1,4) for num in d):
+        #     e1+=1
+        # if all(num < 0 and num > -4 for num in d):
+        #     e1+=1
+
+        a = [num in range (1,4) for num in d]
+        b = [num < 0 and num > -4 for num in d]
+
+        if a.count(False) == 1:
+            a_val = a.index(False)
+            print (f"a_val {a_val}")
+            if a_val == 0 or a_val == -1:
+                e2+=1
+
+        if b.count(False) == 1:
+            b_val = b[b.index(False)]
+            print (f"b_val {b_val}")
+            if b_val == 0 or b_val == -1:
+                e2+=1
+
+        print(n_l)
+        print(d)
+        print(a)
+        print(b)
+
 
     return e1, e2
 
