1dc9304df698c93e084fd903eb5ea51c4c7a81b5
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-12-05 10:59:19 +0000

    another approach for ex2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -6,65 +6,47 @@ def calculate_ex(input_file_name):
     lines = f.readlines()
     
     e1 = 0
-    for l in lines:
-    
+    for l in lines:    
         n = l.split()
         n_l = [int(i) for i in n]
-
-        l_i = n_l.copy()
-        l_i.sort()
-        l_d = n_l.copy()
-        l_d.sort(reverse=True)
-
-        if n_l != l_i and n_l != l_d:
-            continue
-        
-        last = n_l[0]
-        d = []
-        for i in n_l[1:]:
-            delta = abs(i  - last)
-            d.append(delta in range(1, 4))
-            last = i
-        if all(d):
-            e1 += 1
- 
-
-    e2 = e1
-
-    for l in lines:
-    
-        n = l.split()
-        n_l = [int(i) for i in n]
-
         d=[]
         last = n_l[0]
         for i in n_l[1:]:
             d.append(i - last)
             last = i
-        # if all(num in range (1,4) for num in d):
-        #     e1+=1
-        # if all(num < 0 and num > -4 for num in d):
-        #     e1+=1
+        if all(num in range (1,4) for num in d):
+            e1+=1
+        if all(num < 0 and num > -4 for num in d):
+            e1+=1 
 
-        a = [num in range (1,4) for num in d]
-        b = [num < 0 and num > -4 for num in d]
+    e2 = e1
+    for l in lines:    
+        n = l.split()
+        n_l = [int(i) for i in n]
+        d=[]
+        last = n_l[0]
+        for i in n_l[1:]:
+            d.append(i - last)
+            last = i
 
-        if a.count(False) == 1:
-            a_val = a.index(False)
-            print (f"a_val {a_val}")
-            if a_val == 0 or a_val == -1:
-                e2+=1
-
-        if b.count(False) == 1:
-            b_val = b[b.index(False)]
-            print (f"b_val {b_val}")
-            if b_val == 0 or b_val == -1:
-                e2+=1
-
-        print(n_l)
-        print(d)
-        print(a)
-        print(b)
+        a=[]
+        b=[]
+        if all(num >= -1 and num < 4 for num in d):
+            if (d.count(-1) + d.count(0)) == 1:
+                
+                if not [1, -1, 1] in (d[i:i+3] for i in range(len(d) - 2)):
+                  e2+=1  
+                print(n_l)
+                print(d)
+        #a = [num >= -1 and num < 4 for num in d]
+        if all(num <= 1 and num > -4 for num in d):
+             if (d.count(1) + d.count(0)) == 1:
+                if not [1, -1, -1] in (d[i:i+3] for i in range(len(d) - 2)):
+                  e2+=1  
+                
+                print(n_l)
+                print(d)
+    
 
 
     return e1, e2
